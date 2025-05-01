414f9b0119bef0f7d81a28a70f921e9d06ee1c7c
Brilleslangen <nicolaiSivesind@gmail.com>
2025-05-01 20:37:47 +0200

    Added hydra version to avoid warnging and a test to configure the inital model

--- src/train.py
+++ src/train.py
@@ -1,10 +1,10 @@
 import hydra
+import torch
 from omegaconf import OmegaConf, DictConfig
 from hydra.utils import instantiate
-import torch
 
 
-@hydra.main(config_path="../configs", config_name="tasnet_baseline")
+@hydra.main(version_base="1.3", config_path="../configs", config_name="tasnet_baseline")
 def main(cfg: DictConfig):
     model = instantiate(cfg.model_arch)
 
@@ -12,6 +12,15 @@ def main(cfg: DictConfig):
     print(model)
     print("\n=== Effective Configuration ===")
     print(OmegaConf.to_yaml(cfg))
+
+    test_model(model)
+
+
+def test_model(model):
+    x = torch.rand(2, 32000)
+    x = model(x)
+    s1 = x[0]
+    print('Test output shape:', s1.shape)
 
 
 if __name__ == "__main__":
