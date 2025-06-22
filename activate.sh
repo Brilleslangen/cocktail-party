7e0b9aa7936f1475db6dace3e25aa7334fbe5d18
brilleslangen <nicolaisivesind@gmail.com>
2025-06-22 11:59:36 +0200

    Optimizations

--- activate.sh
+++ activate.sh
@@ -4,6 +4,7 @@ module purge
 module load Python/3.11.3-GCCcore-12.3.0
 module load CUDA/12.1.1
 module load cuDNN/8.9.2.26-CUDA-12.1.1
+module load libsndfile/1.0.31-GCCcore-12.3.0
 
 source /cluster/home/nicolts/cocktail-party/venv/bin/activate
 
