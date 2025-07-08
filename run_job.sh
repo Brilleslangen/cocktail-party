2e686ca0bdd52972a7a1000c948a4f4e73ff0aed
Brilleslangen <nicolaiSivesind@gmail.com>
2025-07-08 22:48:13 +0200

    ready for train

--- run_job.sh
+++ run_job.sh
@@ -1,2 +1,2 @@
 export WANDB_ARTIFACT_CACHE_DIR=/cluster/home/nicolts/cocktail-party/artifacts
-python -m src.executables.train --config-name=jobs/train_offline_m hydra/launcher=submitit_idun
+python -m src.executables.train --config-name=jobs/train_streaming_all hydra/launcher=submitit_idun
