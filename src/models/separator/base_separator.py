963bf867af0760baa25562f2cac9dc880ad31036
Brilleslangen <nicolaiSivesind@gmail.com>
2025-07-08 15:00:01 +0200

    Removed relu test

--- src/models/separator/base_separator.py
+++ src/models/separator/base_separator.py
@@ -67,7 +67,7 @@ class BaseSeparator(SubModule):
         self.stateful = self.blocks[0].stateful if n_blocks > 0 else False
 
         # Output projection
-        self.output_proj = nn.Sequential(nn.PReLU(), nn.Linear(d_model, output_dim), nn.ReLU())
+        self.output_proj = nn.Sequential(nn.PReLU(), nn.Linear(d_model, output_dim))
 
     @abstractmethod
     def _build_block(self, block_idx: int) -> nn.Module:
