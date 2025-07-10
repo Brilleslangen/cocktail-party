8f71e5b872a6f706b3816915fe8dfa18e8e70dcb
Brilleslangen <nicolaiSivesind@gmail.com>
2025-07-10 16:58:50 +0200

    eval sh

--- eval_job.sh
+++ eval_job.sh
@@ -1,2 +1,2 @@
 export WANDB_ARTIFACT_CACHE_DIR=/cluster/home/nicolts/cocktail-party/artifacts
-python -m src.executables.train --config-name=jobs/evaluate_offline_all hydra/launcher=submitit_idun
+python -m src.executables.train --config-name=jobs/evaluate_offline_all hydra/launcher=submitit_idun hydra.launcher.timeout_min=30
