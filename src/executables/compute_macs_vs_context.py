a619bbb7798560a662d2306d71f6407214e87e79
Brilleslangen <nicolaiSivesind@gmail.com>
2025-07-17 16:54:42 +0200

    removed l asym

--- src/executables/compute_macs_vs_context.py
+++ src/executables/compute_macs_vs_context.py
@@ -20,7 +20,6 @@ def main(cfg: DictConfig):
     """
     # Models to evaluate
     models_to_eval = ['l-conv-sym', 'l-mamba-sym', 'l-transformer-sym', 'l-liquid-sym',
-                      'l-conv-asym', 'l-mamba-asym', 'l-transformer-asym', 'l-liquid-asym',
                       's-conv-sym', 's-mamba-sym', 's-transformer-sym', 's-liquid-sym',
                       's-conv-asym', 's-mamba-asym', 's-transformer-asym', 's-liquid-asym']
 
