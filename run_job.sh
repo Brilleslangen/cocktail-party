ddd8b20e144a202d14844536dbb5ba93d3645b91
Brilleslangen <nicolaiSivesind@gmail.com>
2025-07-19 03:52:13 +0200

    No energy weighting

--- run_job.sh
+++ run_job.sh
@@ -1,2 +1,2 @@
 export WANDB_ARTIFACT_CACHE_DIR="${WANDB_ARTIFACT_CACHE_DIR:-$HOME/cocktail-party/artifacts}"
-python -m src.executables.train --config-name=jobs/train_ghosts hydra/launcher=submitit_idun
+python -m src.executables.train --config-name=jobs/train_offline_singles hydra/launcher=submitit_idun
